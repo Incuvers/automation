fe2077980885cf030d5f153c58b2a4da81baeacf
cSDes1gn <christian911@sympatico.ca>
2021-03-01 10:38:29 -0500

    remove deprecated docker code

--- docker/post-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is intended to be executed from the context passed to the build script;
-#   which is the directry containing the docker-compose.yml file
-
-# Cleanup dependency list files
-echo "Post Build Step: Cleaning up requirements lists";
-rm ./_raspi/dependencies-apt.conf
-rm ./_raspi/requirements.txt
--- docker/pre-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script is intended to be executed from the context passed to the build script;
-#   which is the directry containing the docker-compose.yml file
-
-# Get dependency list files
-echo "Pre Build Step: Gathering requirements lists";
-python3 get_dependencies.py
-cp ../requirements.txt ./_raspi/requirements.txt;
