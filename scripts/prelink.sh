f49a40596078697a0116eab1deef9077279d174e
cSDes1gn <christian911@sympatico.ca>
2021-03-03 18:34:22 -0500

    update prelink script

--- scripts/prelink.sh
+++ scripts/prelink.sh
@@ -9,13 +9,11 @@ source .env
 trap 'handler $?' ERR
 
 handler() {
-    # use existance of local key file to check for idempotence
-    if [ -f "$LOCAL_KEY_PATH" ]; then
-        printf "%b" "${FAIL} ✗ ${NC} prelink step failed\n"
-        exit $1
-    fi
+    printf "%b" "${FAIL} ✗ ${NC} prelink step failed\n"
+    exit "$1"
 }
 
-printf "%b" "${OKB}Appending key from default public key path: $PUB_KEY_PATH to $PRELINK_USER@$PRELINK_IP:/.ssh/authorized_keys${NC}"
-cat "$PRELINK_KEY_PATH" | ssh "$PRELINK_USER"@"$PRELINK_IP" "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys"
+printf "%b" "${OKB}Appending key from default public key path: $PUB_KEY_PATH to $PRELINK_USER@$PRELINK_IP:/.ssh/authorized_keys${NC}\n"
+KEY=$(< "$PRELINK_KEY_PATH")
+ssh "$PRELINK_USER"@"$PRELINK_IP" "mkdir -p ~/.ssh && echo $KEY >> ~/.ssh/authorized_keys"
 printf "%b" "${OKG} ✓ ${NC} complete\n"
