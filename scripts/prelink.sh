041232c526d34bccb24b31acae0ab08a72472395
cSDes1gn <christian911@sympatico.ca>
2021-03-03 18:41:23 -0500

    resolve on server side

--- scripts/prelink.sh
+++ scripts/prelink.sh
@@ -13,7 +13,6 @@ handler() {
     exit "$1"
 }
 
-printf "%b" "${OKB}Appending key from default public key path: $PUB_KEY_PATH to $PRELINK_USER@$PRELINK_IP:/.ssh/authorized_keys${NC}\n"
-KEY=$(< "$PRELINK_KEY_PATH")
-ssh "$PRELINK_USER"@"$PRELINK_IP" "mkdir -p ~/.ssh && echo \$KEY >> ~/.ssh/authorized_keys"
+printf "%b" "${OKB}Appending local public key to $PRELINK_USER@$PRELINK_IP:/.ssh/authorized_keys${NC}\n"
+ssh "$PRELINK_USER"@"$PRELINK_IP" "mkdir -p ~/.ssh && echo $PRELINK_KEY >> ~/.ssh/authorized_keys"
 printf "%b" "${OKG} ✓ ${NC} complete\n"
